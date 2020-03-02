0bb0e74131e45c94ded3317369e29007a604afa5
Administrator <Administrator@USER-PC>
2020-03-02 11:05:50 +0800

    Initial

---
+++ eLeave/eLeave/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eLeave.Startup))]
+namespace eLeave
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
